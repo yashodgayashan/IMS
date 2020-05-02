6d2d25dedbd4bef1b2d3f9dd7faaacd1b4b466c2
yashodgayashan <yashodgayashan@gmail.com>
2020-05-02 10:56:14 +0530

    Added data-scripts to the backend

---
+++ Backend/resources/database-scripts/data-scripts/first_script.sql
@@ -0,0 +1,5 @@
+-- Insert values to Students table
+INSERT INTO students
+    (id, name, email, active)
+VALUES
+    (1, hasini, h@gmail.com, true);
---
+++ Backend/resources/database-scripts/data-scripts/second_script.sql
@@ -0,0 +1,17 @@
+-- Insert values to student table
+INSERT INTO student
+    (id , name , email , password, role)
+VALUES
+    ("164124V", "Hasini", "h@g.com", "123", "student");
+
+-- Insert values to admin table
+INSERT INTO admin
+    (id , name , email , password, role)
+VALUES
+    ("a-a", "sonali", "s@g.com", "123", "admin");
+
+-- Insert values to organization table
+INSERT INTO organization
+    (id , name , email , password, role)
+VALUES
+    ("virtusa", "v", "v@g.com", "123", "organization");
