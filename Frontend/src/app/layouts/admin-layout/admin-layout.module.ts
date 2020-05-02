aa983e395eaecd3c3179e774b9136d33ced72539
hasinichathu <164124V@uom.lk>
2020-05-02 06:28:58 +0530

    frontend modifcation

--- Frontend/src/app/layouts/admin-layout/admin-layout.module.ts
+++ Frontend/src/app/layouts/admin-layout/admin-layout.module.ts
@@ -5,19 +5,6 @@ import { AdminLayoutRoutingModule } from './admin-layout-routing.module';
 import { FormsModule } from '@angular/forms';
 
 import { DashboardComponent }       from './pages/dashboard/dashboard.component';
-// import { UserComponent }            from './pages/user/user.component';
-// import { TableComponent }           from './pages/table/table.component';
-// import { TypographyComponent }      from './pages/typography/typography.component';
-// import { IconsComponent }           from './pages/icons/icons.component';
-// import { MapsComponent }            from './pages/maps/maps.component';
-// import { NotificationsComponent }   from './pages/notifications/notifications.component';
-// import { UpgradeComponent }         from './pages/upgrade/upgrade.component';
-// import { SelectedStudentsComponent } from './pages/selected-students/selected-students.component';
-// import { NotSelectedStudentsComponent } from './pages/not-selected-students/not-selected-students.component';
-// import { OrganizationsComponent } from './pages/organizations/organizations.component';
-// import { MonthlyReportsComponent } from './pages/monthly-reports/monthly-reports.component';
-// import { FeedbackComponent } from './pages/feedback/feedback.component';
-
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FeedbackComponent } from './pages/feedback/feedback.component';
 import { SelectedStudentsComponent } from './pages/selected-students/selected-students.component';
@@ -35,18 +22,6 @@ import { UserComponent } from './pages/user/user.component';
     MonthlyReportsComponent,
     OrganizationsComponent,
     UserComponent,
-    // UserComponent,
-    // TableComponent,
-    // UpgradeComponent,
-    // TypographyComponent,
-    // IconsComponent,
-    // MapsComponent,
-    // NotificationsComponent,
-    // SelectedStudentsComponent,
-    // NotSelectedStudentsComponent,
-    // OrganizationsComponent,
-    // MonthlyReportsComponent,
-    // FeedbackComponent
   ],
   imports: [
     CommonModule,
