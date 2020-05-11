743dc72155b8cef8bde99ed8edf24bf926709c2b
yashodgayashan <yashodgayashan@gmail.com>
2020-05-11 15:47:45 +0530

    Added student dummy data

--- BackendNew/scripts/dumy-data.sql
+++ BackendNew/scripts/dumy-data.sql
@@ -58,3 +58,23 @@ INSERT INTO Vacancy (CompanyId, InternBatchId, Vacancies, Filled)
 VALUES (2, 1, 12, 11);
 INSERT INTO Vacancy (CompanyId, InternBatchId, Vacancies, Filled) 
 VALUES (3, 2, 10, 10);
+
+/*
+    Add 4 students as dummy data
+*/
+INSERT INTO Student (StudentId, BatchId, CreatedBy, RoleId, FullName, NameWithInitials, Batch, PhoneNumber, Sem1GPA, Sem2GPA, Sem3GPA, Sem4GPA, SGPA, 
+                    Email, CV, PreferedArea1, PreferedArea2, PreferedArea3, DateOfStart, Degree, Password) 
+VALUES (1, 1, 2, 3, "Hasini Senanayaka", "H C Senanayaka", "Batch16", 0713847631, 4.02, 3.98, 4.01, 3.87, 3.99, "hasinisenanayaka@gmail.com", 
+                    "Software Engineering", "Networking", "UI/UX", DATE '2019-08-19', "IT", "hasini");
+INSERT INTO Student (StudentId, BatchId, CreatedBy, RoleId, FullName, NameWithInitials, Batch, PhoneNumber, Sem1GPA, Sem2GPA, Sem3GPA, Sem4GPA, SGPA, 
+                    Email, CV, PreferedArea1, PreferedArea2, PreferedArea3, DateOfStart, Degree, Password) 
+VALUES (2, 1, 2, 3, "Piyumi Dasnayaka", "D A P Dasanayaka", "Batch16", 0717382928, 4.01, 3.48, 4.11, 3.87, 3.87, "piyumimadhubashini@gmail.com", 
+                    "QA Engineering", "Networking", "UI/UX", DATE '2019-08-19', "ITM", "piyumi");
+INSERT INTO Student (StudentId, BatchId, CreatedBy, RoleId, FullName, NameWithInitials, Batch, PhoneNumber, Sem1GPA, Sem2GPA, Sem3GPA, Sem4GPA, SGPA, 
+                    Email, CV, PreferedArea1, PreferedArea2, PreferedArea3, DateOfStart, Degree, Password) 
+VALUES (3, 2, 1, 3, "Lakshika Swarnamali", "A L Swaranamali", "Batch16", 071298765, 4.01, 3.78, 4.13, 3.77, 3.79, "lakshikaswarnamali@gmail.com", 
+                    "Software Engineering", "QA Engineering", "UI/UX", DATE '2019-08-19', "ITM", "lakshika");
+INSERT INTO Student (StudentId, BatchId, CreatedBy, RoleId, FullName, NameWithInitials, Batch, PhoneNumber, Sem1GPA, Sem2GPA, Sem3GPA, Sem4GPA, SGPA, 
+                    Email, CV, PreferedArea1, PreferedArea2, PreferedArea3, DateOfStart, Degree, Password) 
+VALUES (4, 2, 2, 3, "Yashod Perera", "U L Y G Perera", "Batch16", 0716931384, 3.89, 3.78, 4.03, 3.57, 3.72, "hasinisenanayaka@gmail.com", 
+                    "UI/UX", "Networking", "QA Engineering", DATE '2019-08-19', "IT", "yashod");
