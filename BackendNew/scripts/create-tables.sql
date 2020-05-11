e178c7ff52025584eeb8a0e6231fafd406b86b5c
yashodgayashan <yashodgayashan@gmail.com>
2020-05-11 11:54:08 +0530

    Added admin table creation script

--- BackendNew/scripts/create-tables.sql
+++ BackendNew/scripts/create-tables.sql
@@ -1,10 +1,16 @@
-CREATE TABLE Roles
-(
-    RoleId int NOT NULL
-    AUTO_INCREMENT,
-    Description varchar
-    (255) NOT NULL,
-    PRIMARY KEY
-    (RoleId)
+CREATE TABLE Role (
+    RoleId int NOT NULL AUTO_INCREMENT,
+    Description varchar (255) NOT NULL,
+    PRIMARY KEY (RoleId)
 );
 
+CREATE TABLE Admin (
+    AdminId int NOT NULL AUTO_INCREMENT,
+    RoleId int,
+    Name varchar(255) NOT NULL,
+    PhoneNumber int,
+    Email varchar(255),
+    Password varchar(255) NOT NULL,
+    PRIMARY KEY (AdminId),
+    FOREIGN KEY (RoleId) REFERENCES Role(RoleId)
+);
