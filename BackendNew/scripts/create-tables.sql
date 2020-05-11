915fcb912eadd148bb82b7ec75bd724a9329db16
yashodgayashan <yashodgayashan@gmail.com>
2020-05-11 12:02:17 +0530

    Added company table creation script

--- BackendNew/scripts/create-tables.sql
+++ BackendNew/scripts/create-tables.sql
@@ -14,3 +14,20 @@ CREATE TABLE Admin (
     PRIMARY KEY (AdminId),
     FOREIGN KEY (RoleId) REFERENCES Role(RoleId)
 );
+
+CREATE TABLE Company (
+    CompanyId int NOT NULL AUTO_INCREMENT,
+    CreatedBy int NOT NULL,
+    RoleId int NOT NULL,
+    Name varchar(255) NOT NULL,
+    Location varchar(255),
+    Description varchar(255),
+    ContactPerson varchar(255),
+    PhoneNumber int,
+    Website varchar(255),
+    Email varchar(255),
+    Password varchar(255) NOT NULL,
+    PRIMARY KEY (CompanyId),
+    FOREIGN KEY (RoleId) REFERENCES Role(RoleId),
+    FOREIGN KEY (CreatedBy) REFERENCES Admin(AdminId)
+);
