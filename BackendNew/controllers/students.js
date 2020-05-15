159b48b0ddaa99190df1410d9b9b7a235089e5b7
yashodgayashan <yashodgayashan@gmail.com>
2020-05-15 22:01:36 +0530

    Added create student controller function

--- BackendNew/controllers/students.js
+++ BackendNew/controllers/students.js
@@ -68,6 +68,68 @@ exports.getStudent = (req, res) => {
   });
 };
 
+exports.createBasicStudent = (req, res) => {
+  // Validate request
+  if (!req.body) {
+    res.status(400);
+    res.send({
+      message: "Content can not be empty!"
+    });
+  } else if (!req.body.batchId) {
+    res.status(400);
+    res.send({
+      message: "BatchId cannot be empty"
+    });
+  } else if (!req.body.createdBy) {
+    res.status(400);
+    res.send({
+      message: "Please log as admin"
+    });
+  } else if (!req.body.email) {
+    res.status(400);
+    res.send({
+      message: "Invalid email address"
+    });
+  } else if (!req.body.nameWithInitials) {
+    res.status(400);
+    res.send({
+      message: "Name cannot be empty"
+    });
+  } else if (!req.body.password) {
+    res.status(400);
+    res.send({
+      message: "Password cannot be empty"
+    });
+  } else if (!req.body.indexNumber) {
+    res.status(400);
+    res.send({
+      message: "Index number cannot be empty"
+    });
+  }
+
+  // Create a student
+  const student = new BasicStudent({
+    batchId: req.body.batchId,
+    createdBy: req.body.createdBy,
+    studentId: req.body.studentId,
+    email: req.body.email,
+    nameWithInitials: req.body.nameWithInitials,
+    indexNumber: req.body.indexNumber,
+    password: req.body.password
+  });
+
+  students.createBasicStudent(student, (err, result) => {
+    if (err) {
+      console.error("Error :" + err);
+      res.status(404);
+    } else {
+      console.log("Get Student control function");
+      res.status(200);
+      res.send(result.data);
+    }
+  });
+};
+
 const BasicStudent = function(student) {
   this.batchId = student.batchId;
   this.createdBy = student.createdBy;
