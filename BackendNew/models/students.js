9b6cfa699d1cf1d5802ce0091a5ead7e0f088e20
yashodgayashan <yashodgayashan@gmail.com>
2020-05-15 17:34:00 +0530

    Developed getStudent resource function

--- BackendNew/models/students.js
+++ BackendNew/models/students.js
@@ -3,7 +3,7 @@ const sql = require("../models/database.js");
 
 exports.getStudents = callback => {
   var sqlString =
-    "SELECT S.NameWithInitials as Name, S.Batch as RegNo, B.Name as InternBatch FROM Student S, Batch B WHERE S.BatchId = B.BatchId";
+    "SELECT S.NameWithInitials as Name, S.IndexNumber as RegNo, B.Name as InternBatch FROM Student S, Batch B WHERE S.BatchId = B.BatchId";
   sql.query(sqlString, (err, result) => {
     if (err) {
       callback(err, null);
@@ -16,10 +16,10 @@ exports.getStudents = callback => {
 exports.getSelectedStudents = (val, callback) => {
   // Sql query for not selected students.
   var notSelectedStudentsSql =
-    "SELECT S.NameWithInitials as Name, S.Batch as RegNo, B.Name as InternBatch, SC.StudentId FROM Student S, Batch B, Student_Select_Company SC, Company C WHERE S.BatchId = B.BatchId AND S.StudentId = SC.StudentId AND C.CompanyId = SC.CompanyId AND SC.IsSelected = 0 AND S.NameWithInitials NOT IN (SELECT S.NameWithInitials FROM Student S, Batch B, Student_Select_Company SC, Company C WHERE S.BatchId = B.BatchId AND S.StudentId = SC.StudentId AND C.CompanyId = SC.CompanyId AND SC.IsSelected = 1) GROUP BY S.NameWithInitials, S.Batch, B.Name, SC.StudentId";
+    "SELECT S.NameWithInitials as Name, S.IndexNumber as RegNo, B.Name as InternBatch, SC.StudentId FROM Student S, Batch B, Student_Select_Company SC, Company C WHERE S.BatchId = B.BatchId AND S.StudentId = SC.StudentId AND C.CompanyId = SC.CompanyId AND SC.IsSelected = 0 AND S.NameWithInitials NOT IN (SELECT S.NameWithInitials FROM Student S, Batch B, Student_Select_Company SC, Company C WHERE S.BatchId = B.BatchId AND S.StudentId = SC.StudentId AND C.CompanyId = SC.CompanyId AND SC.IsSelected = 1) GROUP BY S.NameWithInitials, S.IndexNumber, B.Name, SC.StudentId";
   // Sql query for selected students.
   var selectedStudentsSql =
-    "SELECT S.NameWithInitials as Name, S.Batch as RegNo, B.Name as InternBatch, SC.StudentId FROM Student S, Batch B, Student_Select_Company SC," +
+    "SELECT S.NameWithInitials as Name, S.IndexNumber as RegNo, B.Name as InternBatch, SC.StudentId FROM Student S, Batch B, Student_Select_Company SC," +
     "Company C WHERE S.BatchId = B.BatchId AND S.StudentId = SC.StudentId AND C.CompanyId = SC.CompanyId AND SC.IsSelected = 1";
   var sqlString;
   if (val == "true") {
@@ -44,9 +44,9 @@ exports.getSelectedStudents = (val, callback) => {
 };
 
 exports.getStudentsByCompany = (comapnyName, callback) => {
-  // Get Selected students by compan
+  // Get Selected students by company.
   var sqlString =
-    "SELECT S.NameWithInitials as Name, S.Batch as RegNo, B.Name as InternBatch, SC.StudentId FROM Student S, Batch B, Student_Select_Company SC, Company C " +
+    "SELECT S.NameWithInitials as Name, S.IndexNumber as RegNo, B.Name as InternBatch, SC.StudentId FROM Student S, Batch B, Student_Select_Company SC, Company C " +
     " WHERE S.BatchId = B.BatchId AND S.StudentId = SC.StudentId AND C.CompanyId = SC.CompanyId AND SC.IsSelected = 1 AND C.Name = ?";
   sql.query(sqlString, [comapnyName], (err, result) => {
     if (err) {
@@ -57,3 +57,15 @@ exports.getStudentsByCompany = (comapnyName, callback) => {
   });
 };
 
+exports.getStudent = (studentId, callback) => {
+  var sqlString =
+    "SELECT S.FullName, S.NameWithInitials, S.IndexNumber, S.PhoneNumber, S.Sem1GPA, S.Sem2GPA, S.Sem3GPA, S.Sem4GPA, S.SGPA, S.Email, S.CV, S.PreferedArea1, " +
+    "S.PreferedArea2, S.PreferedArea3, S.DateOfStart, S.Degree, B.Name FROM Student S, Batch B WHERE S.BatchId = B.BatchId AND S.StudentId = 2;";
+  sql.query(sqlString, [studentId], (err, result) => {
+    if (err) {
+      callback(err, null);
+    } else {
+      callback(null, { data: result });
+    }
+  });
+};
