78f1fd4a6fad4fa1a21c59d446baf588e3ced57a
yashodgayashan <yashodgayashan@gmail.com>
2020-05-19 08:24:42 +0530

    Updated dumy-data.sql

--- BackendNew/scripts/dumy-data.sql
+++ BackendNew/scripts/dumy-data.sql
@@ -6,23 +6,23 @@
     Add several roles to the system including super role, admin role, student role and company role
 */
 -- Super Role
-INSERT INTO Role (RoleId, Name, Description) VALUES (1, 'Super', 'The person who can added any student, company, admin');
+INSERT INTO Role ( Name, Description) VALUES ( 'Super', 'The person who can added any student, company, admin');
 -- Admin Role
-INSERT INTO Role (RoleId, Name, Description) VALUES (2, 'Admin', 'The person who can added any student, company');
+INSERT INTO Role ( Name, Description) VALUES ( 'Admin', 'The person who can added any student, company');
 -- Student Role
-INSERT INTO Role (RoleId, Name, Description) VALUES (3, 'Student', 'The person who is a student');
+INSERT INTO Role ( Name, Description) VALUES ( 'Student', 'The person who is a student');
 -- Company Role
-INSERT INTO Role (RoleId, Name, Description) VALUES (4, 'Company', 'The person who is a company');
+INSERT INTO Role ( Name, Description) VALUES ( 'Company', 'The person who is a company');
 
 /*
     Add two admins who are assigned to super role and the admin role
 */
 -- Super role admin
 INSERT INTO Admin (AdminId, RoleId, Name, PhoneNumber, Email, Password) 
-VALUES (1, 1, "Amila Perera", 0773456746, "amilaperera@gmail.com", "amila");
+VALUES ('amila', 'Super', "Amila Perera", 0773456746, "amilaperera@gmail.com", "amila");
 -- Admin role admin
 INSERT INTO Admin (AdminId, RoleId, Name, PhoneNumber, Email, Password) 
-VALUES (2, 1, "Kasun Fonseka", 0715383948, "kasunfonseka@gmail.com", "kasun");
+VALUES ('kasun', 'Admin', "Kasun Fonseka", 0715383948, "kasunfonseka@gmail.com", "kasun");
 
 /*
     Create three companies naming virtusa, wso2, creative software
