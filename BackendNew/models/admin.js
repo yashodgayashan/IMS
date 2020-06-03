b4496a57dd982794686851c8344eb3fd7d2f4c61
yashodgayashan <yashodgayashan@gmail.com>
2020-06-03 09:10:28 +0530

    Resolved role issue.

--- BackendNew/models/admin.js
+++ BackendNew/models/admin.js
@@ -3,7 +3,7 @@ const sql = require("../models/database.js");
 
 exports.getAdminByUserNamePassword = (adminId, password, callback) => {
   var sqlString =
-    "SELECT AdminId as Id, RoleName as Role, Name FROM Admin WHERE AdminId = ? AND password = ?";
+    "SELECT AdminId as Id, 'Admin' as Role, Name FROM Admin WHERE AdminId = ? AND password = ?";
   sql.query(sqlString, [adminId, password], (err, result) => {
     if (err) {
       callback(err, null);
