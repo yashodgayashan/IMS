6de0f6a6f985b92f562608c074a3b6047887935e
yashodgayashan <yashodgayashan@gmail.com>
2020-06-04 12:05:39 +0530

    developed getSelectedStudents model function

--- BackendNew/models/students.js
+++ BackendNew/models/students.js
@@ -190,6 +190,124 @@ exports.getSelectedStudentsByCompanyBatch = (batchId, companyId, callback) => {
   });
 };
 
+exports.getSelectedStudents = (isSelected, callback) => {
+  if (isSelected == "true") {
+    var sqlString = `
+    SELECT
+        S.IndexNumber,
+        S.FullName,
+        S.NameWithInitials,
+        S.PhoneNumber,
+        S.Email,
+        S.Sem1GPA,
+        S.Sem2GPA,
+        S.Sem3GPA,
+        S.Sem4GPA,
+        S.SGPA,
+        B.BatchId,
+        Sc.IsSelected,
+        C.CompanyId,
+        C.Name as CompanyName
+    FROM
+        student S,
+        student_has_batch H,
+        Batch B,
+        student_select_company SC,
+        Company C
+    WHERE
+        s.IndexNumber = H.IndexNumber
+        AND H.BatchId = B.BatchId
+        AND SC.IndexNumber = S.IndexNumber
+        AND SC.BatchId = B.BatchId
+        AND SC.IsSelected = 1
+        AND C.CompanyId = SC.CompanyId
+    GROUP BY
+        S.IndexNumber,
+        S.FullName,
+        S.NameWithInitials,
+        S.PhoneNumber,
+        S.Email,
+        S.Sem1GPA,
+        S.Sem2GPA,
+        S.Sem3GPA,
+        S.Sem4GPA,
+        S.SGPA,
+        B.BatchId,
+        SC.IsSelected,
+        C.CompanyId,
+        C.Name`;
+    sql.query(sqlString, (err, result) => {
+      if (err) {
+        callback(err, null);
+      } else {
+        callback(null, { data: result });
+      }
+    });
+  } else if (isSelected == "false") {
+    var sqlString = `
+    SELECT
+            S.IndexNumber,
+            S.FullName,
+            S.NameWithInitials,
+            S.PhoneNumber,
+            S.Email,
+            S.Sem1GPA,
+            S.Sem2GPA,
+            S.Sem3GPA,
+            S.Sem4GPA,
+            S.SGPA,
+            B.BatchId,
+            NULL as IsSelected,
+            NULL as CompanyId,
+            NULL as CompanyName
+        from
+            student S,
+            student_has_batch H,
+            Batch B
+        where
+            s.IndexNumber = H.IndexNumber
+            AND H.BatchId = B.BatchId
+            AND s.IndexNumber NOT IN (
+                SELECT
+                    S.IndexNumber
+                from
+                    student S,
+                    student_has_batch H,
+                    Batch B,
+                    student_select_company SC
+                where
+                    s.IndexNumber = H.IndexNumber
+                    AND H.BatchId = B.BatchId
+                    AND SC.IndexNumber = S.IndexNumber
+                    AND SC.BatchId = B.BatchId
+                    AND SC.IsSelected = 1
+            )
+        GROUP BY
+            S.IndexNumber,
+            S.FullName,
+            S.NameWithInitials,
+            S.PhoneNumber,
+            S.Email,
+            S.Sem1GPA,
+            S.Sem2GPA,
+            S.Sem3GPA,
+            S.Sem4GPA,
+            S.SGPA,
+            B.BatchId
+    `;
+    sql.query(sqlString, (err, result) => {
+      if (err) {
+        callback(err, null);
+      } else {
+        callback(null, { data: result });
+      }
+    });
+  } else {
+    err = Error("Invalid query param");
+    callback(err, null);
+  }
+};
+
 exports.createBasicStudent = (student, callback) => {
   var sqlString = "INSERT INTO student SET ?";
   sql.query(sqlString, student, (err, result) => {
