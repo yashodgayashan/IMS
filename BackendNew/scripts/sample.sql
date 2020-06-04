60a6de7ec051f12471395b6bbc4bbd4e765e1eae
yashodgayashan <yashodgayashan@gmail.com>
2020-06-04 11:31:35 +0530

    Added get students by batch script

--- BackendNew/scripts/sample.sql
+++ BackendNew/scripts/sample.sql
@@ -1,104 +1,4 @@
--- Get not selected students
-SELECT
-    S.NameWithInitials as Name,
-    S.IndexNumber as RegNo,
-    B.Name as InternBatch
-FROM
-    Student S,
-    Batch B
-WHERE
-    S.BatchId = B.BatchId
-    AND S.NameWithInitials NOT IN (
-        SELECT
-            S.NameWithInitials
-        FROM
-            Student S,
-            Batch B,
-            Student_Select_Company SC
-        WHERE
-            S.BatchId = B.BatchId
-            AND S.StudentId = SC.StudentId
-            AND SC.IsSelected = 1
-    )
-GROUP BY
-    S.NameWithInitials,
-    S.IndexNumber,
-    B.Name
-    /*Get selected Students*/
-SELECT
-    S.NameWithInitials as Name,
-    S.IndexNumber as RegNo,
-    B.Name as InternBatch,
-    SC.StudentId
-FROM
-    Student S,
-    Batch B,
-    Student_Select_Company SC
-WHERE
-    S.BatchId = B.BatchId
-    AND S.StudentId = SC.StudentId
-    AND SC.IsSelected = 1
-    /*Get All Students*/
-SELECT
-    S.NameWithInitials as Name,
-    S.IndexNumber as RegNo,
-    B.Name as InternBatch
-FROM
-    Student S,
-    Batch B
-WHERE
-    S.BatchId = B.BatchId
-    /* Get Selected students by company*/
-SELECT
-    S.NameWithInitials as Name,
-    S.IndexNumber as RegNo,
-    B.Name as InternBatch,
-    SC.StudentId
-FROM
-    Student S,
-    Batch B,
-    Student_Select_Company SC,
-    Company C
-WHERE
-    S.BatchId = B.BatchId
-    AND S.StudentId = SC.StudentId
-    AND C.CompanyId = SC.CompanyId
-    AND SC.IsSelected = 1
-    AND C.Name = ?
-    /*Check whether company exsists*/
-SELECT
-    COUNT(Name)
-FROM
-    Company
-WHERE
-    Name = "?"
-    /*Get a student by Id*/
-SELECT
-    S.FullName,
-    S.NameWithInitials,
-    S.IndexNumber,
-    S.PhoneNumber,
-    S.Sem1GPA,
-    S.Sem2GPA,
-    S.Sem3GPA,
-    S.Sem4GPA,
-    S.SGPA,
-    S.Email,
-    S.CV,
-    S.PreferedArea1,
-    S.PreferedArea2,
-    S.PreferedArea3,
-    S.DateOfStart,
-    S.Degree,
-    B.Name
-FROM
-    Student S,
-    Batch B
-WHERE
-    S.BatchId = B.BatchId
-    AND S.StudentId = ?
-    
--- Get a student by username and password
+/* Get a student by username and password*/
 SELECT
     IndexNumber as Id,
     RoleName as Role,
@@ -107,9 +7,8 @@ FROM
     Student
 WHERE
     indexNumber = ?
-    AND password = ? 
-
--- Get a company by company id and password
+    AND password = ?
+    /*Get a company by company id and password*/
 SELECT
     companyId as Id,
     RoleName as Role,
@@ -118,9 +17,8 @@ FROM
     Company
 WHERE
     companyId = ?
-    AND password = ? 
-    
--- Get an admin by admin id and password
+    AND password = ?
+    /*Get an admin by admin id and password*/
 SELECT
     AdminId as Id,
     RoleName as Role,
@@ -130,3 +28,104 @@ FROM
 WHERE
     AdminId = ?
     AND password = ?
+    /*Get student by batch*/
+    (
+        SELECT
+            S.IndexNumber,
+            S.FullName,
+            S.NameWithInitials,
+            S.PhoneNumber,
+            S.Email,
+            S.Sem1GPA,
+            S.Sem2GPA,
+            S.Sem3GPA,
+            S.Sem4GPA,
+            S.SGPA,
+            B.BatchId,
+            NULL as IsSelected,
+            NULL as CompanyId,
+            NULL as CompanyName
+        from
+            student S,
+            student_has_batch H,
+            Batch B
+        where
+            s.IndexNumber = H.IndexNumber
+            AND H.BatchId = B.BatchId
+            AND B.BatchId = ?
+            AND s.IndexNumber NOT IN (
+                SELECT
+                    S.IndexNumber
+                from
+                    student S,
+                    student_has_batch H,
+                    Batch B,
+                    student_select_company SC
+                where
+                    s.IndexNumber = H.IndexNumber
+                    AND H.BatchId = B.BatchId
+                    AND SC.IndexNumber = S.IndexNumber
+                    AND SC.BatchId = B.BatchId
+                    AND B.BatchId = ?
+                    AND SC.IsSelected = 1
+            )
+        GROUP BY
+            S.IndexNumber,
+            S.FullName,
+            S.NameWithInitials,
+            S.PhoneNumber,
+            S.Email,
+            S.Sem1GPA,
+            S.Sem2GPA,
+            S.Sem3GPA,
+            S.Sem4GPA,
+            S.SGPA,
+            B.BatchId
+    )
+UNION
+(
+    SELECT
+        S.IndexNumber,
+        S.FullName,
+        S.NameWithInitials,
+        S.PhoneNumber,
+        S.Email,
+        S.Sem1GPA,
+        S.Sem2GPA,
+        S.Sem3GPA,
+        S.Sem4GPA,
+        S.SGPA,
+        B.BatchId,
+        Sc.IsSelected,
+        C.CompanyId,
+        C.Name as CompanyName
+    from
+        student S,
+        student_has_batch H,
+        Batch B,
+        student_select_company SC,
+        Company C
+    where
+        s.IndexNumber = H.IndexNumber
+        AND H.BatchId = B.BatchId
+        AND SC.IndexNumber = S.IndexNumber
+        AND SC.BatchId = B.BatchId
+        AND SC.IsSelected = 1
+        AND C.CompanyId = SC.CompanyId
+        AND B.BatchId = ?
+    GROUP BY
+        S.IndexNumber,
+        S.FullName,
+        S.NameWithInitials,
+        S.PhoneNumber,
+        S.Email,
+        S.Sem1GPA,
+        S.Sem2GPA,
+        S.Sem3GPA,
+        S.Sem4GPA,
+        S.SGPA,
+        B.BatchId,
+        SC.IsSelected,
+        C.CompanyId,
+        C.Name
+)
