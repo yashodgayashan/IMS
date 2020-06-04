574874b62b8dc5d95691718d7807a3a9066dca3d
yashodgayashan <yashodgayashan@gmail.com>
2020-06-04 12:17:23 +0530

    Developed getStudents controller function

--- BackendNew/controllers/students.js
+++ BackendNew/controllers/students.js
@@ -4,13 +4,50 @@ const companies = require("../models/company");
 exports.getStudents = (req, res) => {
   var isSelected = req.query.isSelected;
   var company = req.query.company;
+  var batch = req.query.batch;
 
-  if (isSelected) {
-    // Get selected or not selected students
+  if (batch && isSelected) {
+    students.getSelectedStudentsByBatch(isSelected, batch, (err, result) => {
+      if (err) {
+        console.error("Error :" + err.message);
+        res.status(404).send();
+      } else {
+        console.log("Get Student control function");
+        res.status(200);
+        res.send(result.data);
+      }
+    });
+  } else if (batch && company) {
+    students.getSelectedStudentsByCompanyBatch(
+      batch,
+      company,
+      (err, result) => {
+        if (err) {
+          console.error("Error :" + err.message);
+          res.status(404).send();
+        } else {
+          console.log("Get Student control function");
+          res.status(200);
+          res.send(result.data);
+        }
+      }
+    );
+  } else if (batch) {
+    students.getStudentsByBatch(batch, (err, result) => {
+      if (err) {
+        console.error("Error :" + err.message);
+        res.status(404).send();
+      } else {
+        console.log("Get Student control function");
+        res.status(200);
+        res.send(result.data);
+      }
+    });
+  } else if (isSelected) {
     students.getSelectedStudents(isSelected, (err, result) => {
       if (err) {
         console.error("Error :" + err.message);
-        res.status(404);
+        res.status(404).send();
       } else {
         console.log("Get Student control function");
         res.status(200);
@@ -18,33 +55,21 @@ exports.getStudents = (req, res) => {
       }
     });
   } else if (company) {
-    // Get students by companywise
-    companies.hasCompany(company, (err, result) => {
+    students.getSelectedStudentsByCompany(company, (err, result) => {
       if (err) {
         console.error("Error :" + err.message);
-        res.status(404);
-      } else if (result) {
-        students.getStudentsByCompany(company, (err, result) => {
-          if (err) {
-            console.error("Error :" + err.message);
-            res.status(404);
-          } else {
-            console.log("Get Student control function");
-            res.status(200);
-            res.send(result.data);
-          }
-        });
+        res.status(404).send();
       } else {
-        console.error("Error : Not found a company");
-        res.status(404);
+        console.log("Get Student control function");
+        res.status(200);
+        res.send(result.data);
       }
     });
   } else {
-    //get list of all students
     students.getStudents((err, result) => {
       if (err) {
-        console.error("Error :" + err);
-        res.status(404);
+        console.error("Error :" + err.message);
+        res.status(404).send();
       } else {
         console.log("Get Student control function");
         res.status(200);
