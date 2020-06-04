7cd5cbcf7345c0e4f977f6f5bb57a723c8bffd3b
yashodgayashan <yashodgayashan@gmail.com>
2020-06-04 12:08:22 +0530

    developed getSelectedStudentsByCompany model function

--- BackendNew/models/students.js
+++ BackendNew/models/students.js
@@ -308,6 +308,61 @@ exports.getSelectedStudents = (isSelected, callback) => {
   }
 };
 
+exports.getSelectedStudentsByCompany = (companyId, callback) => {
+  var sqlString = `
+      SELECT
+      S.IndexNumber,
+      S.FullName,
+      S.NameWithInitials,
+      S.PhoneNumber,
+      S.Email,
+      S.Sem1GPA,
+      S.Sem2GPA,
+      S.Sem3GPA,
+      S.Sem4GPA,
+      S.SGPA,
+      B.BatchId,
+      Sc.IsSelected,
+      C.CompanyId,
+      C.Name as CompanyName
+    from
+      student S,
+      student_has_batch H,
+      Batch B,
+      student_select_company SC,
+      Company C
+    where
+      s.IndexNumber = H.IndexNumber
+      AND H.BatchId = B.BatchId
+      AND SC.IndexNumber = S.IndexNumber
+      AND SC.BatchId = B.BatchId
+      AND SC.IsSelected = 1
+      AND C.CompanyId = SC.CompanyId
+      AND C.CompanyId = ?
+    GROUP BY
+      S.IndexNumber,
+      S.FullName,
+      S.NameWithInitials,
+      S.PhoneNumber,
+      S.Email,
+      S.Sem1GPA,
+      S.Sem2GPA,
+      S.Sem3GPA,
+      S.Sem4GPA,
+      S.SGPA,
+      B.BatchId,
+      SC.IsSelected,
+      C.CompanyId,
+      C.Name`;
+  sql.query(sqlString, [companyId], (err, result) => {
+    if (err) {
+      callback(err, null);
+    } else {
+      callback(null, { data: result });
+    }
+  });
+};
+
 exports.createBasicStudent = (student, callback) => {
   var sqlString = "INSERT INTO student SET ?";
   sql.query(sqlString, student, (err, result) => {
