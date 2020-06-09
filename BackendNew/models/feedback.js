b935130ee89e7d8eec6ab4543815a19a5da6d4ed
yashodgayashan <yashodgayashan@gmail.com>
2020-06-09 13:25:03 +0530

    Developed feedback model

--- BackendNew/models/feedback.js
+++ BackendNew/models/feedback.js
@@ -24,3 +24,34 @@ exports.getMultipleFeedbackByBatch = (studentId, batchId, sendFeedbacks) => {
     }
   });
 };
+
+exports.Feedback = function(feedback) {
+  this.FeedbackId = feedback.FeedbackId;
+  this.BatchId = feedback.BatchId;
+  this.IndexNumber = feedback.IndexNumber;
+  this.SubmittedDate = feedback.SubmittedDate;
+  this.CompanyId = feedback.CompanyId;
+  this.StartDate = feedback.StartDate;
+  this.ProjectBrief = feedback.ProjectBrief;
+  this.Mentor = feedback.Mentor;
+  this.IsGivenAdequateWork = feedback.IsGivenAdequateWork;
+  this.WorkEnvironment = feedback.WorkEnvironment;
+  this.EmployeeSupportiveness = feedback.EmployeeSupportiveness;
+  this.ManagementSupportiveness = feedback.ManagementSupportiveness;
+  this.SeniorEngineerCount = feedback.SeniorEngineerCount;
+  this.isPayed = feedback.isPayed;
+  this.Payment = feedback.Payment;
+  this.Problems = feedback.Problems;
+  this.Suggesions = feedback.Suggesions;
+  this.OverallComment = feedback.OverallComment;
+  this.Satisfaction = feedback.Satisfaction;
+  this.IsRecommended = feedback.IsRecommended;
+  this.ContactPerson = feedback.ContactPerson;
+  this.ContactPersonPhoneNumber = feedback.ContactPersonPhoneNumber;
+  this.ContactPersonEmail = feedback.ContactPersonEmail;
+  this.ContactPersonDesignation = feedback.ContactPersonDesignation;
+  this.MentorName = feedback.MentorName;
+  this.MentorPhoneNumber = feedback.MentorPhoneNumber;
+  this.MentorEmail = feedback.MentorEmail;
+  this.MentorDesgnation = feedback.MentorDesgnation;
+};
