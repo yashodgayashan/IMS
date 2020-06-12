41e3ed2801461a34be16a4bfbb4b28df8a424df2
yashodgayashan <yashodgayashan@gmail.com>
2020-06-13 04:53:35 +0530

    Developed getReportsBybatch model function

--- BackendNew/models/monthlyReports.js
+++ BackendNew/models/monthlyReports.js
@@ -1,13 +1,25 @@
 // Import database connection
 const sql = require("./database.js");
 
-exports.getReports = (studentId, sendFeedback) => {
+exports.getReports = (studentId, sendReports) => {
   var sqlString = "SELECT * FROM Monthly_report WHERE IndexNumber = ?";
   sql.query(sqlString, [studentId], (err, result) => {
     if (err) {
-      sendFeedback(err, null);
+      sendReports(err, null);
     } else {
-      sendFeedback(null, { data: result });
+      sendReports(null, { data: result });
+    }
+  });
+};
+
+exports.getReportsByBatch = (studentId, batchId, sendReports) => {
+  var sqlString =
+    "SELECT * FROM Monthly_report WHERE IndexNumber = ? AND BatchId = ?";
+  sql.query(sqlString, [studentId, batchId], (err, result) => {
+    if (err) {
+      sendReports(err, null);
+    } else {
+      sendReports(null, { data: result });
     }
   });
 };
