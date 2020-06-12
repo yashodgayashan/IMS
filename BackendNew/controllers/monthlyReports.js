477c318851961349c64315cd798ceecad6454e0d
yashodgayashan <yashodgayashan@gmail.com>
2020-06-13 04:56:00 +0530

    Updated getReports controller function

--- BackendNew/controllers/monthlyReports.js
+++ BackendNew/controllers/monthlyReports.js
@@ -3,11 +3,21 @@ const reports = require("../models/monthlyReports");
 exports.getReports = (req, res) => {
   var studentId = req.params.studentId;
   var batch = req.query.batch;
-  reports.getReports(studentId, (err, result) => {
-    if (err) {
-      res.status(500).send({ message: "Internal Server Error" });
-    } else {
-      res.status(200).send({ reports: result.data });
-    }
-  });
+  if (batch) {
+    reports.getReportsByBatch(studentId, batch, (err, result) => {
+      if (err) {
+        res.status(500).send({ message: "Internal Server Error" });
+      } else {
+        res.status(200).send({ reports: result.data });
+      }
+    });
+  } else {
+    reports.getReports(studentId, (err, result) => {
+      if (err) {
+        res.status(500).send({ message: "Internal Server Error" });
+      } else {
+        res.status(200).send({ reports: result.data });
+      }
+    });
+  }
 };
