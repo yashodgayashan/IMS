621f4d9d5a90ab0ab495a6afbbb2b0010f014c92
yashodgayashan <yashodgayashan@gmail.com>
2020-06-12 08:17:43 +0530

    Developed removeConfirmation controller function

--- BackendNew/controllers/confirmation.js
+++ BackendNew/controllers/confirmation.js
@@ -66,3 +66,62 @@ exports.createConfirmation = (req, res) => {
     res.status(400).send({ message: "Please send the companyId" });
   }
 };
+
+exports.removeConfirmation = (req, res) => {
+  var studentId = req.params.studentId;
+  var batch = req.query.batch;
+  var company = req.query.company;
+  if (batch && company) {
+    student.hasStudentABatch(studentId, batch, (err, hasBatch) => {
+      if (err) {
+        res.status(500).send({ message: "Internal Server Error" });
+      } else {
+        console.log(hasBatch.data);
+        if (hasBatch.data == 0) {
+          res
+            .status(400)
+            .send({ message: "Student don't register for such batch" });
+        } else {
+          confirmation.hasConfirmedCompany(
+            studentId,
+            batch,
+            company,
+            (err, result) => {
+              if (err) {
+                res.status(500).send({ message: "Internal Server Error" });
+              } else {
+                if (result.data == 1) {
+                  confirmation.removeConfirmation(
+                    studentId,
+                    batch,
+                    company,
+                    (err, result) => {
+                      if (err) {
+                        res
+                          .status(500)
+                          .send({ message: "Internal Server Error" });
+                      } else {
+                        res
+                          .status(200)
+                          .send({ confirmation: "Remove confirmation" });
+                      }
+                    }
+                  );
+                } else {
+                  res.status(400).send({
+                    message:
+                      "Student is not confirmed for the company or student is not yet confirmed"
+                  });
+                }
+              }
+            }
+          );
+        }
+      }
+    });
+  } else if (!batch) {
+    res.status(400).send({ message: "Please send the batchId" });
+  } else {
+    res.status(400).send({ message: "Please send the companyId" });
+  }
+};
