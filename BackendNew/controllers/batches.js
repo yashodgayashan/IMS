8451101dc4efd0b440ec4cc8af2919216e3c59cf
yashodgayashan <yashodgayashan@gmail.com>
2020-07-06 13:50:12 +0530

    Developed createBatch controller function

--- BackendNew/controllers/batches.js
+++ BackendNew/controllers/batches.js
@@ -9,3 +9,33 @@ exports.getBatches = (req, res) => {
     }
   });
 };
+
+exports.createBatch = (req, res) => {
+  if (req.user.Role == "Admin") {
+    checkBatch(req, (err, msg) => {
+      if (err) {
+        res.status(400).send({ message: msg });
+      } else {
+        const batch = new batches.Batch({
+          BatchId: req.body.batchId,
+          CreatedBy: req.user.Id,
+          Year: req.body.year,
+          StartDate: req.body.startDate,
+          EndDate: req.body.endDate,
+          CompanyCount: req.body.allowedCompanyCount
+        });
+        batches.createBatch(batch, (err, result) => {
+          if (err) {
+            res.status(500).send({ message: "Internal Server Error" });
+          } else {
+            res.status(201).send({
+              batchId: batch.BatchId
+            });
+          }
+        });
+      }
+    });
+  } else {
+    res.status(403).send("Unauthorized");
+  }
+};
