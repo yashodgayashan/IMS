654a04f46921d49d702c6934890a4d191e1e5074
yashodgayashan <yashodgayashan@gmail.com>
2020-07-06 13:49:50 +0530

    Developed getBatches controller function

--- BackendNew/controllers/batches.js
+++ BackendNew/controllers/batches.js
@@ -1,1 +1,11 @@
+const batches = require("../models/batches");
 
+exports.getBatches = (req, res) => {
+  batches.getBatches((err, result) => {
+    if (err) {
+      res.status(500).send({ message: "Internal Server Error" });
+    } else {
+      res.status(200).send({ data: result });
+    }
+  });
+};
